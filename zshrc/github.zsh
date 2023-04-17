a7f137e1cb3c9352e2493f44e16661fb8759b6e0
Ryota-Onuma <onumaryotajh.tech@gmail.com>
2023-04-17 09:56:07 +0900

    feat: ✨ update

--- zshrc/github.zsh
+++ zshrc/github.zsh
@@ -1,19 +1,19 @@
 #　未レビューのPRをすべて取得
 fetchReviewAssignedPRs () {
     # export GITHUB_REVIEW_TARGET_REPOS=("user/repo" "user/repo")
-    results=()
+    is_hit=false
     for r in ${GITHUB_REVIEW_TARGET_REPOS} 
     do
       result=$(gh pr list --search "review-requested:@me repo:${r}" --json number,title,updatedAt --template '{{range .}}{{tablerow (printf "#%v" .number | autocolor "green") .title (timeago .updatedAt)}}{{end}}')
       if [[ -n "${result}" ]]; then
-        results+=("\n\n[${r}]\n")
-        results+=("$result")
+        echo "\n[${r}]\n"
+        echo "$result"
+        echo "\n"
+        is_hit=true
       fi
     done
 
-    if [[ ${#results[@]} -eq 0 ]]; then
-        results+=("\nNo PR is ready to be reviewd.\n")
+    if [[ ${is_hit} == false ]]; then
+        echo "\nNo PR is ready to be reviewd.\n"
     fi
-
-    echo "${results[@]}"
 }
